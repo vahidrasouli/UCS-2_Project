919d97503110da605fe99aa1dd3153e21445cd69
vahidrasouli <vstakr@gmail.com>
2018-09-09 18:02:11 +0430

    update & fix code -->[decode_ucs2]
    Signed-off-by: vahidrasouli <vstakr@gmail.com>

--- file.py
+++ file.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-ucs2_string = str('"064506270646062F0647002006270639062A062806270631000A00330039003000360033003000200631064A06270644"' \
-                  .replace('"', '').decode("hex"))
+ucs2_string = '"064506270646062F0647002006270639062A062806270631000A00330039003000360033003000200631064A06270644"'
 
-def decode_ucs2(invite):
+def decode_ucs2(data, divider):
     decoded_data = None
     try:
-        out_data = unicode(invite, "utf-16-be").encode("utf8")  # CONVERT UCS2 TO STRING
+        data = str(data.replace(divider, '').decode("hex"))
+        out_data = unicode(data, "utf-16-be").encode("utf8")  # CONVERT UCS2 TO STRING
     except:
         out_data = invite
         print "couldn't decode: {}".format(out_data)
@@ -27,7 +27,7 @@ def encode_ucs2(invite):
     finally:
         return decoded_data
 
-A_out = decode_ucs2(ucs2_string)
+A_out = decode_ucs2(ucs2_string,'"')
 print A_out
 
 B_out = encode_ucs2(u'سلام')
