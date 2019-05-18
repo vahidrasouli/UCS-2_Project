ce87c3d41662c1a7be678d3f6b592e77cebc921e
vahidrasouli <vstakr@gmail.com>
2019-05-18 17:51:47 +0000

    debug and fix code.

--- file.py
+++ file.py
@@ -3,18 +3,20 @@
 
 ucs2_string = '"064506270646062F0647002006270639062A062806270631000A00330039003000360033003000200631064A06270644"'
 
+
 def decode_ucs2(data, divider):
     decoded_data = None
     try:
         data = str(data.replace(divider, '').decode("hex"))
         out_data = unicode(data, "utf-16-be").encode("utf8")  # CONVERT UCS2 TO STRING
     except:
-        out_data = invite
+        out_data = data
         print "couldn't decode: {}".format(out_data)
     else:
         decoded_data = out_data
     finally:
         return decoded_data
+
 
 def encode_ucs2(invite):
     decoded_data = None
@@ -28,7 +30,8 @@ def encode_ucs2(invite):
     finally:
         return decoded_data
 
-A_out = decode_ucs2(ucs2_string,'"')
+
+A_out = decode_ucs2(ucs2_string, '"')
 print A_out
 
 B_out = encode_ucs2(u'سلام')
