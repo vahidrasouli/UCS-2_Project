f74a0ae3e6ea7ea5fdbdb8cc965be5f670a618d7
vahidrasouli <vstakr@gmail.com>
2019-05-19 17:45:23 +0000

    update code & support python 2.7, 3.7

--- file.py
+++ file.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+import codecs
+
 ucs2_string = '"064506270646062F0647002006270639062A062806270631000A00330039003000360033003000200631064A06270644"'
 
 
 def decode_ucs2(data, divider):
+    """
+    Decode first level: hex encrypt, second level: ucs2[utf-16-be] and finally to text[utf8].
+    :param data: your input str. sample: [0633064406270645]
+    :param divider: your text divider - sample['"'] and finally replace ''.
+    :return: finally return encode text[utf8] - sample: [سلام]
+    """
     decoded_data = None
     try:
-        data = str(data.replace(divider, '').decode("hex"))
-        out_data = unicode(data, "utf-16-be").encode("utf8")  # CONVERT UCS2 TO STRING
+        data = data.replace(divider, '')
+        out_data = codecs.decode(data, 'hex').decode('utf-16-be')  # CONVERT UCS2 TO STRING
     except:
         out_data = data
-        print "couldn't decode: {}".format(out_data)
+        print("couldn't decode: {}".format(out_data))
     else:
         decoded_data = out_data
     finally:
@@ -19,12 +27,18 @@ def decode_ucs2(data, divider):
 
 
 def encode_ucs2(invite):
+    """
+    Encode text to first level: ucs2[utf-16-be] and finally to hex encrypt.
+    :param invite: your input str. sample: [سلام]
+    :return: hex encrypt. sample: [0633064406270645]
+    """
     decoded_data = None
     try:
-        out_data = unicode(invite).encode("utf-16-be").encode("hex")  # CONVERT STRING TO UCS2
+        data = codecs.encode(invite, 'utf-16-be')   # CONVERT STRING TO UCS2
+        out_data = codecs.encode(data, 'hex')   # CONVERT UCS2 TO HEX
     except:
         out_data = invite
-        print "couldn't decode: {}".format(out_data)
+        print("couldn't decode: {}".format(out_data))
     else:
         decoded_data = out_data
     finally:
@@ -32,7 +46,7 @@ def encode_ucs2(invite):
 
 
 A_out = decode_ucs2(ucs2_string, '"')
-print A_out
+print(A_out)
 
 B_out = encode_ucs2(u'سلام')
-print B_out
+print(B_out)
